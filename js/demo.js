3468b2212f312c0e91dff3fb7fe59eb9dd2d4482
gusFuentes <gusfuentes1@gmail.com>
2016-09-27 21:20:48 -0500

    General Revision of styles, some images added

--- js/demo.js
+++ js/demo.js
@@ -10,12 +10,8 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet', '$mdSidenav', '$mdDialog'
         controller: DialogController,
         template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName" placeholder="Placeholder text"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="theMax"> </md-input-container> </div> <md-input-container flex> <label>Address</label> <input ng-model="user.address"> </md-input-container> <div layout layout-sm="column"> <md-input-container flex> <label>City</label> <input ng-model="user.city"> </md-input-container> <md-input-container flex> <label>State</label> <input ng-model="user.state"> </md-input-container> <md-input-container flex> <label>Postal Code</label> <input ng-model="user.postalCode"> </md-input-container> </div> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-dialog-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
         targetEvent: ev,
+        clickOutsideToClose : true,
       })
-      .then(function(answer) {
-        $scope.alert = 'You said the information was "' + answer + '".';
-      }, function() {
-        $scope.alert = 'You cancelled the dialog.';
-      });
   };
 }]);
 
@@ -35,20 +31,20 @@ function DialogController($scope, $mdDialog) {
 app.config(function($mdThemingProvider) {
 
   $mdThemingProvider.definePalette('amazingPaletteName', {
-    '50': 'fff',
-    '100': 'ffcdd2',
-    '200': 'ef9a9a',
-    '300': 'e57373',
-    '400': 'ef5350',
+    '50': '000',
+    '100': '000',
+    '200': '000',
+    '300': '000',
+    '400': '000',
     '500': '000',
-    '600': 'e53935',
-    '700': 'd32f2f',
-    '800': 'c62828',
-    '900': 'b71c1c',
-    'A100': 'ff8a80',
-    'A200': 'ff5252',
-    'A400': 'ff1744',
-    'A700': 'd50000',
+    '600': '000',
+    '700': '000',
+    '800': '000',
+    '900': '000',
+    'A100': '000',
+    'A200': '000',
+    'A400': '000',
+    'A700': '000',
     'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
                                         // on this palette should be dark or light
 
@@ -59,7 +55,9 @@ app.config(function($mdThemingProvider) {
 
   $mdThemingProvider.theme('default')
     .primaryPalette('amazingPaletteName')
-  .accentPalette('purple', {
-      'default': '500' // use shade 200 for default, and keep all other shades the same
+  .accentPalette('amazingPaletteName', {
+      'default': '100' // use shade 200 for default, and keep all other shades the same
     });
 });
+
+
