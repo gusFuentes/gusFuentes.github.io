756dcfee518a0f6440dc0e796922d13f0a65cc1d
gusFuentes <gusfuentes1@gmail.com>
2016-10-03 12:49:14 -0500

    ng-route implementation with nice directive to avoid md-buttons hard customizations

--- js/demo.js
+++ js/demo.js
@@ -1,4 +1,4 @@
-var app = angular.module('StarterApp', ['ngMaterial', 'ngMdIcons']);
+var app = angular.module('StarterApp', ['ngMaterial', 'ngMdIcons', 'ngRoute']);
 
 app.controller('AppCtrl', ['$scope', '$mdBottomSheet', '$mdSidenav', '$mdDialog', function($scope, $mdBottomSheet, $mdSidenav, $mdDialog) {
   $scope.toggleSidenav = function(menuId) {
@@ -27,56 +27,123 @@ function DialogController($scope, $mdDialog) {
   };
 };
 
+app.directive( 'goClick', function ( $location, $mdSidenav ) {
+  return function ( scope, element, attrs ) {
+    var path;
+
+    attrs.$observe( 'goClick', function (val) {
+      path = val;
+    });
+
+    element.bind( 'click', function () {
+      scope.$apply( function () {
+        $location.path( path );
+      });
+    });
+  };
+});
+
 
 app.config(function($mdThemingProvider) {
-      $mdThemingProvider.definePalette('customPalette', {
-        '50': 'ffebee',
-        '100': 'ffffff',
-        '200': 'ef9a9a',
-        '300': 'e57373',
-        '400': 'ef5350',
-        '500': 'f44336',
-        '600': 'ffffff',
-        '700': 'd32f2f',
-        '800': 'c62828',
-        '900': 'b71c1c',
-        'A100': 'ff8a80',
-        'A200': 'ff5252',
-        'A400': 'ff1744',
-        'A700': 'd50000',
-        'contrastDefaultColor': 'light', // whether, by default, text (contrast)
-        // on this palette should be dark or light
-        'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-          '200', '300', '400', 'A100'
-        ],
-        'contrastLightColors': undefined // could also specify this if default was 'dark'
-      });
+$mdThemingProvider.definePalette('customPalette', {
+	'50': 'ffebee',
+	'100': 'ffffff',
+	'200': 'ef9a9a',
+	'300': 'e57373',
+	'400': 'ef5350',
+	'500': 'f44336',
+	'600': 'ffffff',
+	'700': 'd32f2f',
+	'800': 'c62828',
+	'900': 'b71c1c',
+	'A100': 'ff8a80',
+	'A200': 'ff5252',
+	'A400': 'ff1744',
+	'A700': 'd50000',
+	'contrastDefaultColor': 'light', // whether, by default, text (contrast)
+	// on this palette should be dark or light
+	'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+	  '200', '300', '400', 'A100'
+	],
+	'contrastLightColors': undefined // could also specify this if default was 'dark'
+});
 
-      /**** Declaring custom theme and extended "indigo" theme with hues ****/
+/**** Declaring custom theme and extended "indigo" theme with hues ****/
 
-      $mdThemingProvider.theme('default')
-        .primaryPalette('customPalette', {
-          'default': '400',
-          'hue-1': '100',
-          'hue-2': '600',
-          'hue-3': 'A100'
-        })
-        .accentPalette('cyan', {
-          'default': '200'
-        })
-        .warnPalette('indigo', {
-          'default': '500'
-        });
+$mdThemingProvider.theme('default')
+	.primaryPalette('customPalette', {
+	  'default': '400',
+	  'hue-1': '100',
+	  'hue-2': '600',
+	  'hue-3': 'A100'
+})
+.accentPalette('cyan', {
+  'default': '200'
+})
+.warnPalette('indigo', {
+  'default': '500'
+});
 
-      /**** declaring multiple themes ****/
+/**** declaring multiple themes ****/
 
-      $mdThemingProvider.theme('altTheme')
-        .primaryPalette('teal')
+$mdThemingProvider.theme('altTheme')
+.primaryPalette('teal')
 
-      $mdThemingProvider.alwaysWatchTheme(true); // enable the default theme to be used across your entire application
+$mdThemingProvider.alwaysWatchTheme(true); // enable the default theme to be used across your entire application
 
-      // $mdThemingProvider.setDefaultTheme('altTheme'); // change the default theme to custom theme
+// $mdThemingProvider.setDefaultTheme('altTheme'); // change the default theme to custom theme
 
-    });
+});
+
+app.config(function($routeProvider) {
+  $routeProvider
+
+  .when('/', {
+    templateUrl : 'pages/home.html',
+    controller  : 'HomeController'
+  })
+
+  .when('/works', {
+    templateUrl : 'pages/works.html',
+    controller  : 'WorksController'
+  })
+
+  .when('/videogames', {
+    templateUrl : 'pages/videogames.html',
+    controller  : 'VideogamesController'
+  })
+
+  .when('/experiments', {
+    templateUrl : 'pages/experiments.html',
+    controller  : 'ExperimentsController'
+  })
+
+  .when('/mixedmedia', {
+    templateUrl : 'pages/mixedmedia.html',
+    controller  : 'MixedmediaController'
+  })
+
+  .otherwise({redirectTo: '/'});
+});
+
+app.controller('HomeController', function($scope) {
+  $scope.message = 'Hello from HomeController';
+});
+
+app.controller('WorksController', function($scope) {
+  $scope.message = 'Hello from WorksController';
+});
+
+app.controller('VideogamesController', function($scope) {
+  $scope.message = 'Hello from VideogamesController';
+});
+
+app.controller('ExperimentsController', function($scope) {
+  $scope.message = 'Hello from ExperimentsController';
+});
+
+app.controller('MixedmediaController', function($scope) {
+  $scope.message = 'Hello from MixedmediaController';
+});
 
 
