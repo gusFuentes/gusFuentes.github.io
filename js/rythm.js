af22a036d93f92c52ce4831aacd883f1509004e2
gusFuentes <gusfuentes1@gmail.com>
2016-12-12 14:30:17 -0500

    Rythm v0.2

--- js/rythm.js
+++ js/rythm.js
@@ -4,8 +4,9 @@ var goodText = document.getElementById("goodIndicator");
 var intervalText = document.getElementById("intervalIndicator");
 var goodOrBadText = document.getElementById("goodOrBad");
 var differenceText = document.getElementById("difference");
+var hitButton = document.getElementById("hitButton");
 
-var beatTimes = [ 100, 200, 300, 400 ];
+var beatTimes = [ 250, 500, 750, 1000 ];
 var ranges = [];
 var prueba = 0;
 var timeLimit;
@@ -28,7 +29,7 @@ function updateRanges(){
       ranges[i][2] = beatTimes[i];
       ranges[i][3] = beatTimes[i] + percentage(beatTimes[i], goodRange);
       ranges[i][4] = beatTimes[i] + ((beatTimes[i + 1] - beatTimes[i])/2) - 1;
-      rangesText.innerHTML = rangesText.innerHTML + ranges[i] + "<br>";
+      //rangesText.innerHTML = rangesText.innerHTML + ranges[i] + "<br>";
     }
     else if(i == beatTimes.length - 1){ //Last
       ranges[i][0] = beatTimes[i] - ((beatTimes[i] - beatTimes[i - 1])/2);
@@ -37,7 +38,7 @@ function updateRanges(){
       ranges[i][3] = beatTimes[i] + percentage( (beatTimes[i] - beatTimes[i - 1]), goodRange);
       ranges[i][4] = beatTimes[i] + ((beatTimes[i] - beatTimes[i-1])/2) - 1;
       timeLimit = ranges[i][4];
-      rangesText.innerHTML = rangesText.innerHTML + ranges[i] + "<br>";
+      //rangesText.innerHTML = rangesText.innerHTML + ranges[i] + "<br>";
     }    
     else{
       ranges[i][0] = beatTimes[i] - ((beatTimes[i] - beatTimes[i - 1])/2);
@@ -45,21 +46,26 @@ function updateRanges(){
       ranges[i][2] = beatTimes[i];
       ranges[i][3] = beatTimes[i] + percentage( (beatTimes[i + 1] - beatTimes[i]), goodRange);
       ranges[i][4] = beatTimes[i] + ((beatTimes[i + 1] - beatTimes[i])/2) - 1;
-      rangesText.innerHTML = rangesText.innerHTML + ranges[i] + "<br>";
+      //rangesText.innerHTML = rangesText.innerHTML + ranges[i] + "<br>";
     }
   }
 }
-updateRanges();
 
-var clock = setInterval(function() {
+var setClock;
+
+function initClock() {
+    setClock = setInterval(clock, 5);
+}
+
+function clock() {
+    
     prueba ++;
-    var ellapsedTime = prueba;  
+    var ellapsedTime = prueba;
     globalEllapsedtime = ellapsedTime;
     timeText.innerHTML = ellapsedTime
   
     if (ellapsedTime >= timeLimit){
-      console.log("stop");
-      clearInterval(clock);
+      clearInterval(setClock);
     }
     
     for(i = 0; i < ranges.length; i++){
@@ -72,21 +78,25 @@ var clock = setInterval(function() {
        }else if(ellapsedTime > ranges[i][3] && ellapsedTime <= ranges[i][4]){
          //console.log("front" + currentInterval);
          rangeState = 0;
-       }
+       }      
       
        if(ellapsedTime == ranges[i][4] && ellapsedTime != timeLimit){
          console.log("interval" + i);
          currentInterval ++;
          updateIntervalText()
        }
+      
+       if(ellapsedTime == ranges[i][2]){
+         hitButton.innerHTML = i + 1;
+         var beat = new Audio("https://gusfuentes.github.io/sound/4d.wav");
+         beat.play();
+       }
     }
-    
-}, 1);
+}
 
 function updateIntervalText(){
   intervalText.innerHTML = "Current interval: " + beatTimes[currentInterval];
 }
-updateIntervalText();
 
 function takeHit(){
   var difference = Math.abs(beatTimes[currentInterval] - globalEllapsedtime);
@@ -97,10 +107,58 @@ function takeHit(){
 function evalAccuracy(dif){
   if(rangeState == 0){
     goodOrBadText.innerHTML = "Bad";
-    document.getElementById("hitButton").style.backgroundColor = "Red";
+    removeHitBtnClasses();
+    hitButton.classList.add("badAnimate");
   }else{
     goodOrBadText.innerHTML = "Good!";
-    document.getElementById("hitButton").style.backgroundColor = "Green";
+    removeHitBtnClasses();
+    hitButton.classList.add("goodAnimate");
     navigator.vibrate(100);
+  }
+}
+
+function removeHitBtnClasses(){
+  hitButton.classList.remove("goodAnimate");
+  hitButton.classList.remove("badAnimate");
+  void hitButton.offsetWidth;
+}
+
+//INITIAL SETTINGS
+document.getElementById("preload").style.visibility = "hidden";
+document.getElementById("playground").style.visibility = "hidden";
+
+var countdownNumber = 1000;
+var innerCountdownNumber = countdownNumber;
+var countdownInterval;
+
+function startCountdown(){
+  document.getElementById("selectTempo").style.visibility = "hidden";
+  document.getElementById("playground").style.visibility = "visible";
+  updateRanges();
+  countdownInterval = setInterval(countdown, 5);
+}
+
+function countdown() { 
+  if(countdownNumber > 0){
+    countdownNumber--;
+  }else if(countdownNumber == 0){
+    updateIntervalText();
+    initClock();
+    clearInterval(countdownInterval);
+  }
+  var beat = new Audio("https://gusfuentes.github.io/sound/4d.wav");
+
+  if(countdownNumber == (innerCountdownNumber/4) * 3){
+    hitButton.innerHTML = "1";
+    beat.play();
+  }else if(countdownNumber == (innerCountdownNumber/4) * 2){
+    hitButton.innerHTML = "2";
+    beat.play();
+  }else if(countdownNumber == (innerCountdownNumber/4)){
+    hitButton.innerHTML = "3";
+    beat.play();
+  }else if(countdownNumber == 0){
+    hitButton.innerHTML = "4";
+    beat.play();
   }
 }
