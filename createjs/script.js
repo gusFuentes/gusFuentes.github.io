f6dd165fec41441d267dfb008c8d8859f49090ce
gusFuentes <gusfuentes1@gmail.com>
2017-03-19 12:42:13 -0500

    rotation ok

--- createjs/script.js
+++ createjs/script.js
@@ -2,13 +2,33 @@ canvas = document.getElementById("gameCanvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 stage = new createjs.Stage(canvas);
+
 var TO_RADIANS = Math.PI/180; 
 var angle = 0;
+var angleInDegrees;
+var ship_coords = [canvas.width/2, canvas.height/2];
+
+var PrototipoNave = function(){
+	this.image = new Image();
+	this.image.src = "img/spaceship.png";
+	this.bitmap = new createjs.Bitmap(this.image);
+	this.container = new createjs.Container();
+	this.container.addChild(this.bitmap);
+	this.bitmap.x = canvas.width / 2;
+	this.bitmap.y = canvas.height / 2;	
+	this.bitmap.regX = this.image.width / 2 | 0;
+	this.bitmap.regY = this.image.height / 2 | 0;
+	stage.addChild(this.container);
+}
+
+var nuevaNave = new PrototipoNave();
+
 
 canvas.addEventListener("mousemove", function(e){
-		angle = Math.atan2( ship_coords[0] - e.clientX ,ship_coords[1] -  e.clientY  )*-1;
-		console.log("reading");
-	})
+	angle = Math.atan2( nuevaNave.bitmap.x - e.clientX , nuevaNave.bitmap.y -  e.clientY  )*-1;
+	angleInDegrees = angle * (180 / Math.PI);
+	//console.log("angle" + angle);
+});
 
 var messageField;		//Message display field
 messageField = new createjs.Text("Gus Game", "bold 24px Arial", "#000");
@@ -19,26 +39,24 @@ messageField = new createjs.Text("Gus Game", "bold 24px Arial", "#000");
 	messageField.y = canvas.height / 2;
 	stage.addChild(messageField);
 	stage.update();
-var image = new Image();
-	image.src = "img/spaceship.png";
-image.onload = handleImageLoad;
-var update = true;
+createjs.Ticker.addEventListener("tick", tick);
+	createjs.Ticker.setFPS(60);
+
 
 function handleImageLoad(event) {
-	var image = event.target;
-	var bitmap;
+	//var image = event.target;
 	var container = new createjs.Container();
-	stage.addChild(container);
+	//stage.addChild(container);
 
 	// create and populate the screen with random daisies:
-		bitmap = new createjs.Bitmap(image);
+		
 		container.addChild(bitmap);
-		bitmap.x = canvas.width * Math.random() | 0;
-		bitmap.y = canvas.height * Math.random() | 0;
-		bitmap.rotation = 360 * Math.random() | 0;
+		bitmap.x = canvas.width / 2;
+		bitmap.y = canvas.height / 2;
+		
 		bitmap.regX = bitmap.image.width / 2 | 0;
 		bitmap.regY = bitmap.image.height / 2 | 0;
-		bitmap.scaleX = bitmap.scaleY = bitmap.scale = Math.random() * 0.4 + 0.6;
+		//bitmap.scaleX = bitmap.scaleY = bitmap.scale = Math.random() * 0.4 + 0.6;
 		bitmap.name = "bmp_";
 		bitmap.cursor = "pointer";
 
@@ -57,23 +75,9 @@ function handleImageLoad(event) {
 			update = true;
 		});
 
-		bitmap.on("rollover", function (evt) {
-			this.scaleX = this.scaleY = this.scale * 1.2;
-			update = true;
-		});
-
-		bitmap.on("rollout", function (evt) {
-			this.scaleX = this.scaleY = this.scale;
-			update = true;
-		});
-
-	//examples.hideDistractor();
-	createjs.Ticker.addEventListener("tick", tick);
+	
 }
 function tick(event) {
-	// this set makes it so the stage only re-renders when an event handler indicates a change has happened.
-	if (update) {
-		update = false; // only update once
-		stage.update(event);
-	}
+	nuevaNave.bitmap.rotation = angleInDegrees;
+	stage.update();
 }
