cca5657de8388dec47d3cd0d9c7c3857055a202d
gusFuentes <gusfuentes1@gmail.com>
2017-03-20 14:52:10 -0500

    working particles, ship and mouse

--- createjs/script.js
+++ createjs/script.js
@@ -7,6 +7,51 @@ var TO_RADIANS = Math.PI/180;
 var angle = 0;
 var angleInDegrees;
 
+var particles = [];
+var num_particles = 1000;
+
+function GetRandomColor() {
+	var r = 0, g = 0, b = 0;
+
+	while (r < 100 && g < 100 && b < 100){
+		r = Math.floor(Math.random() * 256);
+		g = Math.floor(Math.random() * 256);
+		b = Math.floor(Math.random() * 256);
+	}
+
+	return "rgb(" + r + "," + g + ","  + b + ")";
+};
+
+var Particle = function () {
+	this.vx = 4 * Math.random() - 2;
+	this.vy = 4 * Math.random() - 2;
+	this.Color = GetRandomColor();
+	this.rect = new createjs.Shape();
+	this.rect.graphics.beginFill(this.Color);
+	this.rect.regX = canvas.width * Math.random();
+	this.rect.regY = canvas.width * Math.random();
+	this.rect.x = this.rect.regX;
+	this.rect.y = this.rect.regY;
+	this.rect.graphics.drawRect(this.rect.x, this.rect.y, 4, 4);
+	this.rect.graphics.endFill();
+    stage.addChild(this.rect);
+};
+
+Particle.prototype.Update = function () {
+    this.rect.x += this.vx;
+    this.rect.y += this.vy;
+ 
+    if (this.rect.x<0 || this.rect.x > canvas.width)
+        this.vx = -this.vx;
+ 
+    if (this.rect.y < 0 || this.rect.y > canvas.height)
+        this.vy = -this.vy;
+};
+
+for (var i = 0; i < num_particles; i++){
+    particles.push(new Particle());
+}
+
 var PrototipoNave = function(){
 	this.image = new Image();
 	this.image.src = "img/spaceship.png";
@@ -17,8 +62,8 @@ var PrototipoNave = function(){
 	this.bitmap.y = canvas.height / 2;	
 	this.bitmap.regX = this.image.width / 2 | 0;
 	this.bitmap.regY = this.image.height / 2 | 0;
-	this.bitmap.width = 64;
-	this.bitmap.height = 64;
+	this.bitmap.width = this.image.width;
+	this.bitmap.height = this.image.height;
 	this.vx = 0;
 	this.vy = 0;
 	stage.addChild(this.container);
@@ -51,7 +96,6 @@ PrototipoNave.prototype.UpdateSpeed = function (axys, speed) {
   }else{
     this.vy += speed;
   }
-  console.log("h " + nuevaNave.vx);
 }
 
 var nuevaNave = new PrototipoNave();
@@ -75,8 +119,6 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-
-
 canvas.addEventListener("mousemove", function(e){
 	angle = Math.atan2( nuevaNave.bitmap.x - e.clientX , nuevaNave.bitmap.y -  e.clientY  )*-1;
 	angleInDegrees = angle * (180 / Math.PI);
@@ -88,5 +130,8 @@ createjs.Ticker.setFPS(60);
 function tick(event) {
 	nuevaNave.UpdatePosition(0.016);
 	nuevaNave.bitmap.rotation = angleInDegrees;
+	for (var i = 0; i < num_particles; i++) {
+        particles[i].Update();
+    }
 	stage.update();
 }
