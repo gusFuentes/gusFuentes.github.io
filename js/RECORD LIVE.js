6ba4ff38167da9991f3c240a591670b869a1bc7c
gusFuentes <gusfuentes1@gmail.com>
2020-07-02 11:31:55 -0500

    .

--- js/RECORD LIVE.js
+++ js/RECORD LIVE.js
@@ -55,3 +55,38 @@ function draw(){
 
   //console.log(level);
 }
+
+var KEYCODE_LEFT = 37, 
+KEYCODE_RIGHT = 39,
+KEYCODE_UP = 38, 
+KEYCODE_DOWN = 40,
+KEYCODE_W = 87,
+KEYCODE_A = 65,
+KEYCODE_D = 68,
+KEYCODE_S = 83;
+KEYCODE_X = 88;
+KEYCODE__ = 32;
+
+function keyPressed(event) {
+  switch(event.keyCode) {
+    case KEYCODE_UP:
+      mostrarImagen("url('img/EcosystemMapOSP.png')");
+      break
+    case KEYCODE_DOWN: 
+      mostrarImagen("url('img/participantes.png')");
+      break
+  }
+
+  stage.update();
+}
+
+function mostrarImagen(imgurl) {
+  var x = document.getElementById("imagen");
+  x.style.backgroundImage = imgurl;
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
